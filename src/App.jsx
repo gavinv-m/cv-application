8ee18fcaf0142459ac03aa9cfd4d6d14696a7f8f
Gavin <gavinvm@protonmail.com>
2024-10-29 17:14:13 +0200

    feat: Create contact details

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './sidebar/Header';
 import PersonalDetails from './sidebar/PersonalDetails';
+import ContactDetails from './sidebar/ContactDetails';
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <aside>
         <Header></Header>
         <PersonalDetails></PersonalDetails>
+        <ContactDetails></ContactDetails>
       </aside>
       <main></main>
     </>
--- src/sidebar/ContactDetails.jsx
+++ src/sidebar/ContactDetails.jsx
@@ -1,1 +1,15 @@
-export default function ContactDetails() {}
+export default function ContactDetails() {
+  return (
+    <div className="contact-details">
+      <div className="contact-details-header">
+        {/* TODO: Add svg contact card */}
+        <h2>Contact Details</h2>
+      </div>
+      <input type="email" placeholder="Email" />
+      <div className="cell-location">
+        <input type="tel" placeholder="Enter mobile phone number" />
+        <input type="text" placeholder="City, Country" />
+      </div>
+    </div>
+  );
+}
