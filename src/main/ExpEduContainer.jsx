984620a0b5293c183d28181c6cb3fc8840597c78
Gavin <gavinvm@protonmail.com>
2024-11-05 17:05:34 +0200

    feat: Conditionally render resume main

--- src/main/ExpEduContainer.jsx
+++ src/main/ExpEduContainer.jsx
@@ -2,31 +2,46 @@ export default function ExpEduContainer({ experience, education }) {
   return (
     <div className="exp-edu-container">
       <div className="exp-container">
-        <h3>Experience</h3>
-        <div className="experience-group">
-          {experience.map((exp) => {
-            return (
-              <div className="experience-section" key={exp.key}>
-                <h4>{exp.position}</h4>
-                <h5>{`${exp.company} | ${exp.startDate} - ${exp.endDate}`}</h5>
-                <p>{exp.description}</p>
-              </div>
-            );
-          })}
-        </div>
+        {experience.some(
+          (exp) =>
+            exp.position ||
+            exp.company ||
+            exp.startDate ||
+            exp.endDate ||
+            exp.description,
+        ) && (
+          <>
+            <h3>Experience</h3>
+            <div className="experience-group">
+              {experience.map((exp) => {
+                return (
+                  <div className="experience-section" key={exp.key}>
+                    <h4>{exp.position}</h4>
+                    <h5>{`${exp.company} | ${exp.startDate} - ${exp.endDate}`}</h5>
+                    <p>{exp.description}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
       </div>
       <div className="edu-container">
-        <h3>Education</h3>
-        <div className="education-section-group">
-          {education.map((edu) => {
-            return (
-              <div className="education-section" key={edu.key}>
-                <h4>{edu.school}</h4>
-                <h5>{edu.degree}</h5>
-              </div>
-            );
-          })}
-        </div>
+        {education.some((edu) => edu.school || edu.degree) && (
+          <>
+            <h3>Education</h3>
+            <div className="education-section-group">
+              {education.map((edu) => {
+                return (
+                  <div className="education-section" key={edu.key}>
+                    <h4>{edu.school}</h4>
+                    <h5>{edu.degree}</h5>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- src/main/SkillsCertsContainer.jsx
+++ src/main/SkillsCertsContainer.jsx
@@ -2,20 +2,30 @@ export default function SkillsCertsContainer({ skills, certifications }) {
   return (
     <div className="skills-certs-container">
       <div className="skills-container">
-        <h3>Skills</h3>
-        <ul>
-          {skills.map((skill) => (
-            <li key={skill.key}>{skill.skill}</li>
-          ))}
-        </ul>
+        {skills.some((skill) => skill.skill !== '') && (
+          <>
+            <h3>Skills</h3>
+            <ul>
+              {skills.map((skill) => (
+                <li key={skill.key}>{skill.skill}</li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
       <div className="certifications-container">
-        <h3>Certifications</h3>
-        <ul>
-          {certifications.map((certification) => (
-            <li key={certification.key}>{certification.certification}</li>
-          ))}
-        </ul>
+        {certifications.some(
+          (certification) => certification.certification !== '',
+        ) && (
+          <>
+            <h3>Certifications</h3>
+            <ul>
+              {certifications.map((certification) => (
+                <li key={certification.key}>{certification.certification}</li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     </div>
   );
